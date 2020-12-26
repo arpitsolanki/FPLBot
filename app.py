0231a93b39bb01c63ecea29d1fb9dc654d60aac7
arpitsolanki <arpitsolanki14@gmail.com>
2020-12-26 16:03:25 +0530

    Create app.py
    Streamlit app code

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
+st.beta_set_page_config(**PAGE_CONFIG)
+def main():
+
+  st.title("Fantasy Dream Team Predictions ")
+  st.subheader("Predictions for GameWeek 15")
+  team=pd.read_csv('https://raw.githubusercontent.com/arpitsolanki/FPLBot/main/output.csv')
+  st.dataframe(team)
+
+  menu = ["Gameweek 15"]
+  choice = st.sidebar.selectbox('Menu',menu)
+	# if choice == 'Home':
+	# 	st.subheader("Streamlit From Colab")	
+  
+if __name__ == '__main__':
+	main()
